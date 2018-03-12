0d2fad12760983c8752d425befa125ac0bfa5e31
progre <djyayutto@gmail.com>
2018-03-12 21:23:41 +0900

    feat: YouTube auth refreshing
    #3

--- src/infrastructure/PromisifiedYoutubeAPI.ts
+++ src/infrastructure/PromisifiedYoutubeAPI.ts
@@ -1,65 +1,153 @@
-const youtubeAPI = require('youtube-api');
+// tslint:disable-next-line:no-implicit-dependencies
+import electron from 'electron';
+const electronGoogleOauth = require('electron-google-oauth');
+import fetch from 'node-fetch';
+const encryptor = require('simple-encryptor')('2P54vcTFvrbvf6ga');
 import util from 'util';
+const youtubeAPI = require('youtube-api');
 
-export async function insertVideo(
-  params: any,
-  size: number,
-  progressReceiver: { progress(progress: number): void; },
-) {
-  return new Promise<any>((resolve, reject) => {
-    let timer: any;
-    const req = youtubeAPI.videos.insert(params, (err: any, data: any) => {
-      clearInterval(timer);
-      if (err != null) {
-        err.message += ' (function: insertVideo)';
-        reject(err);
-        return;
-      }
-      resolve(data);
-    });
-    timer = setInterval(
-      () => {
-        progressReceiver.progress(req.req.connection._bytesDispatched / size);
-      },
-      250,
-    );
-  });
+const googleOAuth = electronGoogleOauth(electron.BrowserWindow);
+
+interface AccessToken {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  refresh_token: string;
 }
 
-/**
- * 権限チェック
- */
-export const checkAuth = (() => {
-  const update = util.promisify(youtubeAPI.playlists.update);
-  return async () => {
+export default class PromisifiedYoutubeAPI {
+  // tslint:disable:max-line-length
+  private clientId = encryptor.decrypt('dd8351bd6c4259b262f70aeb9d3f2227a5ed6bdef0c3798882a4d0bb151fdf8a32eccb322d8370c42e26246e9d97ba57jXu8fWCt6UU4bHDuv24SC3o0tcZPY8MiZ067CyuA4GhH/SNPANiN3EvOIl23kIfh1wqCthvYdyG9vuvSXgWO7p8b5qlLBK9/yM7mkPN2Q8A=');
+  private clientSecret = encryptor.decrypt('880090244585df22a14ff228999cad3f7d6a98dc4f21df7de9dffc2a1269e99621703fea2b749e6622ce79a4aa1b1ccfWJiKoyS4V55UILG7uhkq2OuXahxnj2qmBqUZ7K4DGe4=');
+  // tslint:enable:max-line-length
+  private refreshToken!: string;
+
+  async authenticate() {
+    const accessToken = await getAccessToken(this.clientId, this.clientSecret);
+    if (accessToken == null) {
+      return;
+    }
+    youtubeAPI.authenticate({
+      type: 'oauth',
+      token: accessToken.access_token,
+    });
+    this.refreshToken = accessToken.refresh_token;
+  }
+
+  async insertVideo(
+    params: any,
+    size: number,
+    progressReceiver: { progress(progress: number): void; },
+  ): Promise<any> {
     try {
-      await update({ part: 'snippet' });
+      return await new Promise<any>((resolve, reject) => {
+        let timer: any;
+        const req = youtubeAPI.videos.insert(params, (err: any, data: any) => {
+          clearInterval(timer);
+          if (err != null) {
+            err.message += ' (function: insertVideo)';
+            reject(err);
+            return;
+          }
+          resolve(data);
+        });
+        timer = setInterval(
+          () => {
+            progressReceiver.progress(req.req.connection._bytesDispatched / size);
+          },
+          250,
+        );
+      });
     } catch (e) {
-      if (e.code === 401) {
-        // TODO: ビューで食える情報を落としたい
-        // 'Login Required', 'Invalid Credentials'
+      if (e.message !== 'Invalid Credentials') {
+        e.message += ' (function: insertVideo)';
         throw e;
       }
     }
-  };
-})();
+    await refreshAccessToken(this.clientId, this.clientSecret, this.refreshToken);
+    return this.insertVideo(params, size, progressReceiver);
+  }
 
-export const listVideos = promisify('listVideos', youtubeAPI.videos.list);
-export const updateVideo = promisify('updateVideo', youtubeAPI.videos.update);
-export const insertPlaylist = promisify('insertPlaylist', youtubeAPI.playlists.insert);
-export const insertPlaylistItem = promisify('insertPlaylistItem', youtubeAPI.playlistItems.insert);
-export const setThumbnail = promisify('setThumbnail', youtubeAPI.thumbnails.set);
-
-function promisify(name: string, func: Function) {
-  return async (params: any) => new Promise<any>((resolve, reject) => {
-    func(params, (err: any, data: any) => {
-      if (err != null) {
-        console.error(JSON.stringify(err), JSON.stringify(params));
-        err.message += ` (function: ${name})`;
-        reject(err);
-        return;
+  /**
+   * 権限チェック
+   */
+  checkAuth = (() => {
+    const update = util.promisify(youtubeAPI.playlists.update);
+    return async (): Promise<void> => {
+      try {
+        await update({ part: 'snippet' });
+      } catch (e) {
+        if (e.message === 'Login Required') {
+          throw e;
+        }
+        if (e.message !== 'Invalid Credentials') {
+          return;
+        }
       }
-      resolve(data);
-    });
+      await refreshAccessToken(this.clientId, this.clientSecret, this.refreshToken);
+      return this.checkAuth();
+    };
+  })();
+
+  listVideos = this.promisify('listVideos', youtubeAPI.videos.list);
+  updateVideo = this.promisify('updateVideo', youtubeAPI.videos.update);
+  insertPlaylist = this.promisify('insertPlaylist', youtubeAPI.playlists.insert);
+  insertPlaylistItem = this.promisify('insertPlaylistItem', youtubeAPI.playlistItems.insert);
+  setThumbnail = this.promisify('setThumbnail', youtubeAPI.thumbnails.set);
+
+  promisify(name: string, func: Function) {
+    const promisifiedFunc = util.promisify(func);
+    return async (params: any) => {
+      try {
+        return await promisifiedFunc(params);
+      } catch (e) {
+        if (e.message !== 'Invalid Credentials') {
+          console.error(JSON.stringify(e), JSON.stringify(params));
+          e.message += ` (function: ${name})`;
+          throw e;
+        }
+      }
+      await refreshAccessToken(this.clientId, this.clientSecret, this.refreshToken);
+      return this.promisify(name, func);
+    };
+  }
+}
+
+async function refreshAccessToken(clientId: string, clientSecret: string, refreshToken: string) {
+  console.log('refresh', new Error().stack);
+  const res = await fetch('https://accounts.google.com/o/oauth2/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: [
+      `client_id=${clientId}`,
+      `client_secret=${clientSecret}`,
+      `refresh_token=${refreshToken}`,
+      `grant_type=refresh_token`,
+    ].join('&'),
   });
+  const tokens = await res.json();
+  youtubeAPI.authenticate({
+    type: 'oauth',
+    token: tokens.access_token,
+  });
+}
+
+async function getAccessToken(clientId: string, clientSecret: string) {
+  try {
+    return <AccessToken>await googleOAuth.getAccessToken(
+      [
+        'https://www.googleapis.com/auth/youtube',
+        'https://www.googleapis.com/auth/youtube.upload',
+      ],
+      clientId,
+      clientSecret,
+    );
+  } catch (e) {
+    if (e.message === 'User closed the window') {
+      return null;
+    }
+    throw e;
+  }
 }
--- src/infrastructure/Youtube.ts
+++ src/infrastructure/Youtube.ts
@@ -1,20 +1,6 @@
-// tslint:disable-next-line:no-implicit-dependencies
-import electron from 'electron';
-const electronGoogleOauth = require('electron-google-oauth');
-const googleOAuth = electronGoogleOauth(electron.BrowserWindow);
 import fs from 'fs';
-const encryptor = require('simple-encryptor')('2P54vcTFvrbvf6ga');
 import util from 'util';
-import {
-  checkAuth,
-  insertPlaylist,
-  insertPlaylistItem,
-  insertVideo,
-  listVideos,
-  setThumbnail,
-  updateVideo,
-} from './PromisifiedYoutubeAPI';
-const youtubeAPI = require('youtube-api');
+import PromisifiedYoutubeAPI from './PromisifiedYoutubeAPI';
 
 const statFile = util.promisify(fs.stat);
 
@@ -37,15 +23,8 @@ export interface Playlist {
   }>;
 }
 
-interface AccessToken {
-  access_token: string;
-  token_type: string;
-  expires_in: number;
-  refresh_token: string;
-}
-
 export default class Youtube {
-  private accessToken?: AccessToken;
+  private api = new PromisifiedYoutubeAPI();
   private authenticating = false;
 
   async authenticate() {
@@ -53,17 +32,12 @@ export default class Youtube {
       return;
     }
     this.authenticating = true;
-    const accessToken = await getAccessToken();
-    if (accessToken != null) {
-      this.accessToken = accessToken;
-      youtubeAPI.authenticate({
-        type: 'oauth',
-        token: this.accessToken.access_token,
-      });
+    try {
+      await this.api.authenticate();
+    } finally {
+      this.authenticating = false;
     }
-    this.authenticating = false;
   }
-  // TODO: トークンの更新 https://developers.google.com/youtube/v3/guides/auth/server-side-web-apps?hl=ja#OAuth2_Refreshing_a_Token
 
   async uploadVideo(
     filePath: string,
@@ -73,8 +47,8 @@ export default class Youtube {
     progressReceiver: { progress(progress: number): void; },
   ) {
     const { size } = await statFile(filePath);
-    await checkAuth();
-    const { id } = await insertVideo(
+    await this.api.checkAuth();
+    const { id } = await this.api.insertVideo(
       {
         part: 'snippet,status',
         resource: {
@@ -89,7 +63,7 @@ export default class Youtube {
     if (typeof id !== 'string') {
       throw new Error('upload failed');
     }
-    await setThumbnail({
+    await this.api.setThumbnail({
       videoId: id,
       media: { body: fs.createReadStream(thumbnailFilePath) },
     });
@@ -98,7 +72,7 @@ export default class Youtube {
 
   async updateVideo(videoId: string, snippet: Snippet) {
     // preload snippet for categoryId
-    const { items } = await listVideos({
+    const { items } = await this.api.listVideos({
       part: 'snippet',
       id: videoId,
     });
@@ -111,7 +85,7 @@ export default class Youtube {
     Object.keys(cleanSnippet)
       .filter(key => (<any>cleanSnippet)[key] == null)
       .forEach((key) => { delete (<any>cleanSnippet)[key]; });
-    await updateVideo({
+    await this.api.updateVideo({
       part: 'snippet',
       resource: {
         id: videoId,
@@ -121,8 +95,8 @@ export default class Youtube {
   }
 
   async createPlaylist(playlist: Playlist, privacyStatus: PrivacyStatus) {
-    await checkAuth();
-    const { id } = await insertPlaylist({
+    await this.api.checkAuth();
+    const { id } = await this.api.insertPlaylist({
       part: 'snippet,status',
       resource: {
         snippet: {
@@ -137,51 +111,31 @@ export default class Youtube {
       throw new Error('creating playlist failed');
     }
     for (const item of playlist.items) {
-      await createPlaylistItem(id, item.videoId, item.note);
+      await this.createPlaylistItem(id, item.videoId, item.note);
     }
     return id;
   }
-}
 
-async function getAccessToken() {
-  try {
-    return <AccessToken>await googleOAuth.getAccessToken(
-      [
-        'https://www.googleapis.com/auth/youtube',
-        'https://www.googleapis.com/auth/youtube.upload',
-      ],
-      // tslint:disable:max-line-length
-      encryptor.decrypt('dd8351bd6c4259b262f70aeb9d3f2227a5ed6bdef0c3798882a4d0bb151fdf8a32eccb322d8370c42e26246e9d97ba57jXu8fWCt6UU4bHDuv24SC3o0tcZPY8MiZ067CyuA4GhH/SNPANiN3EvOIl23kIfh1wqCthvYdyG9vuvSXgWO7p8b5qlLBK9/yM7mkPN2Q8A='),
-      encryptor.decrypt('880090244585df22a14ff228999cad3f7d6a98dc4f21df7de9dffc2a1269e99621703fea2b749e6622ce79a4aa1b1ccfWJiKoyS4V55UILG7uhkq2OuXahxnj2qmBqUZ7K4DGe4='),
-      // tslint:enable:max-line-length
-    );
-  } catch (e) {
-    if (e.message === 'User closed the window') {
-      return null;
-    }
-    throw e;
+  private async createPlaylistItem(
+    playlistId: string,
+    videoId: string,
+    note: string,
+  ) {
+    const itemParams = {
+      part: 'snippet,contentDetails',
+      resource: {
+        snippet: {
+          playlistId,
+          resourceId: { videoId, kind: 'youtube#video' },
+          // position:,
+        },
+        contentDetails: {
+          note,
+          // startAt:,
+          // endAt:,
+        },
+      },
+    };
+    await this.api.insertPlaylistItem(itemParams);
   }
-}
-
-async function createPlaylistItem(
-  playlistId: string,
-  videoId: string,
-  note: string,
-) {
-  const itemParams = {
-    part: 'snippet,contentDetails',
-    resource: {
-      snippet: {
-        playlistId,
-        resourceId: { videoId, kind: 'youtube#video' },
-        // position:,
-      },
-      contentDetails: {
-        note,
-        // startAt:,
-        // endAt:,
-      },
-    },
-  };
-  await insertPlaylistItem(itemParams);
 }
